8165d3057b13f61f29fa446819fd4033614258b9
JonU238 <Jamestfishes@gmail.com>
2023-01-31 14:57:07 -0500

    Changed distance and alt values

--- Notification.py
+++ Notification.py
@@ -14,11 +14,4 @@ def text_me(msg):
                         to =    important["to#"])#Your phone number
     print(message.sid)
 
-def weather(num):
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=42.291961&lon=-71.329079&exclude=hourly,daily&appid=1ff170bc342cf4ce6ca274567226d2ec"
-    response = requests.request("GET",url)
-    if num==1:
-        return str(response.json()["current"])
-    elif num==2:
-        return "Temp: " + str(round((response.json()["current"]["temp"]-273.15)*(9/5)+32,2)) +"\n"+"Wind speed: "+ str(response.json()["current"]["wind_speed"]) +"\n"+"Wind Gust: "+ str(response.json()["current"]["wind_gust"])
 
